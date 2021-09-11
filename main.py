32b6a5609d889dab641e69b3f0cd46156b96f3cd
Tego Chang <cc703@duke.edu>
2021-09-11 03:42:31 +0000

    change main.py to test CD

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def root():
 async def add (num1: int, num2: int):
     """Add two numbers together"""
     
-    total = num1 + num2
+    total = num1 + 2 * num2
     return {"total": total}
     
 if __name__ == '__main__':
